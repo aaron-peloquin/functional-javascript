df57b89a536a0733a17be7d314dcbf65dbe70165
Aaron <aaron4uk@gmail.com>
2018-09-22 07:51:23 -0500

    changing variables to make all conditions true

--- random-practice/binary.js
+++ random-practice/binary.js
@@ -1,21 +1,26 @@
 let a = 1
 let b = 1
-
 // OR
 if((a | b)) {
   console.log("at least one of the variable is 1")
 }
 
+a = 0
+b = 0
 // Not OR
 if(!(a | b)) {
   console.log("both variable are 0")
 }
 
+a = 1
+b = 1
 // AND
 if((a & b)) {
   console.log("both variables are 1")
 }
 
+a = 0
+b = 1
 // Not AND
 if(!(a & b)) {
   console.log("at least oen variable is 0")
