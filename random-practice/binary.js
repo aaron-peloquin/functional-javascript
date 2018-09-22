b5d151dbefe603b954c59f1f23b64a35542047f5
Aaron <aaron4uk@gmail.com>
2018-09-22 07:45:30 -0500

    adding not or

--- random-practice/binary.js
+++ random-practice/binary.js
@@ -4,6 +4,12 @@ let a = 1
 let b = 0
 
 // OR
-if((a | b)){
+if((a | b)) {
   console.log("at least one of the variable is 1")
 }
+
+// Not OR
+if(!(a | b)) {
+  console.log("both variable are 0")
+}
+
