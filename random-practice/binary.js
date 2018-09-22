ac589d34bc89f577108a2a7bcb5f8321406f0a29
Aaron <aaron4uk@gmail.com>
2018-09-22 07:46:59 -0500

    adding 'and' & 'not and'

--- random-practice/binary.js
+++ random-practice/binary.js
@@ -13,3 +13,17 @@ if(!(a | b)) {
   console.log("both variable are 0")
 }
 
+// AND
+if((a & b)) {
+  console.log("both variables are 1")
+}
+
+// Not AND
+if(!(a & b)) {
+  console.log("at least oen variable is 0")
+}
+
+// XOR
+if((a ^ b)){
+  console.log("varaiables do not match eachother")
+}
