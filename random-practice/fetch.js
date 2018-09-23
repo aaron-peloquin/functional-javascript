23bfa93812a0182ca1b836750da6d5df968cfecb
Aaron <aaron4uk@gmail.com>
2018-09-23 09:37:50 -0500

    returning json data

--- random-practice/fetch.js
+++ random-practice/fetch.js
@@ -1,5 +1,7 @@
 
 let ip_data = fetch("https://ipinfo.io/8.8.8.8/geo")
 
-ip_data.then((resp)=>{console.log(resp)})
+ip_data = ip_data.then((resp)=>resp.json())
+
+ip_data = ip_data.then((json)=>json)
 
