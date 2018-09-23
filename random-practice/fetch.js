3e771479ab1fdaa01434998d996bad01a18f80d9
Aaron <aaron4uk@gmail.com>
2018-09-23 10:01:40 -0500

    fixed variable name, added comments

--- random-practice/fetch.js
+++ random-practice/fetch.js
@@ -1,16 +1,25 @@
-
+/** GET geolocation data for one of Google's DNS servers */
 let response = fetch("https://ipinfo.io/8.8.8.8/geo")
 
+/** Convert response into JSON data */
 json_data = response.then((resp)=>resp.json())
 
+/** Update DOM with JSON data */
 json_data.then((json)=>{
-  let output_ul = document.querySelector('#output')
-  console.log(output_ul)
+  /** Find the output <dl> */
+  let output_dl = document.querySelector('#output')
+
+  /** Loop through all JSON we recieved, and print to the page */
   for(var key in json) {
+    /** Create <dt> and <dd> tags */
     let title = document.createElement('dt')
     let description = document.createElement('dd')
-    output_ul.appendChild(title);
-    output_ul.appendChild(description);
+
+    /** Append <dt> and <dd> to <dl> */
+    output_dl.appendChild(title);
+    output_dl.appendChild(description);
+
+    /** Fill <dt> with key, and <dd> with the value */
     title.innerText = key
     description.innerText = json[key]
   }
