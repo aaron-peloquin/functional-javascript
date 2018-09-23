8be1d920ca1f3d67234af026a8c6c1f47d88f486
Aaron <aaron4uk@gmail.com>
2018-09-23 09:42:32 -0500

    logging more data

--- random-practice/fetch.js
+++ random-practice/fetch.js
@@ -5,5 +5,8 @@ json_data = response.then((resp)=>resp.json())
 
 json_data.then((json)=>{
   console.log("The IP is:",json.ip)
+  console.log("It's city is:",json.city)
+  console.log("It's country is:",json.country)
+  console.log("It's postal code is:",json.postal)
 })
 
