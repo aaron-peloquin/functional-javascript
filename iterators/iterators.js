1ab409c74ebd042131c37f8176cd21893f5ac236
Aaron <aaron4uk@gmail.com>
2018-09-25 14:29:37 -0500

    cleaning up code a little, finished commenting

--- iterators/iterators.js
+++ iterators/iterators.js
@@ -2,26 +2,24 @@
 let numObjects = Math.floor(Math.random()*5)
 const maxObjects = Math.floor(Math.random()*15)+10
 
-/**  */
+/** Create an odd iteratable object */
 const oddObject = {
   [Symbol.iterator]: ()=>{
     return {
       next: () => {
         /** Add a random number to numObjects */
         numObjects += Math.floor(Math.random()*3)+1
-        /** Return numObjects to the iterator,
-         * and if we are greater than the maxObjects,
-         * stop the iterator
-         */
-        return {
-          value: numObjects,
-          done: (numObjects>maxObjects)
-        }
+        /** Return numObjects as the value, and return done boolean if > max */
+        return { value: numObjects, done: (numObjects>maxObjects) }
       }
     }
   }
 }
 
+/** for(.. of ..) loop calls an object's [Symbol.iterator].next() function
+ *  v is set to the next() function's returned .value property
+ *  the loop terminates when the next function returns .done = true
+ */
 for(v of oddObject){
   console.log(v)
 }
