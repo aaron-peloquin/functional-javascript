8a72275f9f82b36a8a1633efeb394b178b71b922
Aaron <aaron4uk@gmail.com>
2018-09-28 13:37:36 -0500

    micro-learning [].every()

---
+++ random-practice/every.js
@@ -0,0 +1,7 @@
+/** -5 is below 0, so 'everything' is not above 0. */
+let all_above_zero = [5,10,-5,20].every((number)=>number>1)
+
+console.log(all_above_zero)
+
+
+/** (https://www.youtube.com/watch?v=ZCxsknqbuwU) */
