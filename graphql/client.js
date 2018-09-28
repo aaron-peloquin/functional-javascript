dbd28dc0cccc0b7902a5cc9d12161bc8caa1af60
Aaron <aaron4uk@gmail.com>
2018-09-28 15:15:36 -0500

    console logs your api key's username using graphql

--- graphql/client.js
+++ graphql/client.js
@@ -1,3 +1,19 @@
-let key = new require('./github-personal-access-key');
+const fetch = require('node-fetch');
+const config = require('./github-config')
 
-console.log("key",key)
+let graphql_query = {"query": "{viewer{login}}"}
+
+let fetch_options = {
+  method: 'POST',
+  headers: {
+    'Authorization': 'bearer ' + config.key,
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(graphql_query),
+}
+let data_promise = fetch('https://api.github.com/graphql', fetch_options)
+
+data_promise.then(res => res.json())
+.then((res) => {
+  console.log("Hello, your name is: ",res.data.viewer.login)
+});
